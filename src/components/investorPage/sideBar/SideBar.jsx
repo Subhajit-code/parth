d4af5b371dc91861e5948cddbfe1e4f40b849dc1
Subhajit Banerjee <subhajit.banerjee.cse2023@tint.edu.in>
2026-02-25 20:26:13 +0530

    fixed investor page

--- src/components/investorPage/sideBar/SideBar.jsx
+++ src/components/investorPage/sideBar/SideBar.jsx
@@ -1,12 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "./SideBar.module.css";
 
 const SideBar = ({ data, handleHeaderChange, currentHeader }) => {
   return (
     <div className={styles.container}>
+      <h2 className={styles.header}>Investor Relations</h2>
       <div className={styles.wrapper}>
-        <h2 className={styles.header}>Investor Relations</h2>
-
         <ul className={styles.listWrapper}>
           {data.map((val, i) => (
             <li
